b44ae07e346fd41c18e8250e065f089521b565e5
John Naylor <jonaylor89@gmail.com>
2019-04-03 17:58:34 -0400

    Getting backbone setup

---
+++ Assignment5/assignment5.lisp
@@ -0,0 +1,33 @@
+
+(defun myList () 
+  (list 4 (list 7 22) "art" (list "math" (list 8) 99) 100)
+)
+
+(defun leapYear ()
+    
+)
+
+(defun union- (l1 l2)
+ 
+)
+
+(defun avg (aList)
+  (cond (aList = NIL))
+                       
+)
+
+(defun isType (dataType) 
+ 
+)
+
+(defun taxCalculator (limit rate values)
+ 
+)
+
+(defun clean (aFunc aList)
+ 
+)
+
+(defun threeWayBranch (x y toExecute)
+ 
+)
