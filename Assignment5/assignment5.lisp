2a648cf704e974084d45c6791243e08c3450c2c4
John Naylor <jonaylor89@gmail.com>
2019-04-09 10:32:40 -0400

    docstrings

--- Assignment5/assignment5.lisp
+++ Assignment5/assignment5.lisp
@@ -1,33 +1,46 @@
 
+; 1
 (defun myList () 
+  "Return the magical list"
   (list 4 (list 7 22) "art" (list "math" (list 8) 99) 100)
 )
 
+; 2
 (defun leapYear ()
-    
+  "Return the leap years between 1800 to 2018"    
 )
 
+; 3
 (defun union- (l1 l2)
- 
+  "Return the union of two lists" 
+
 )
 
+; 4
 (defun avg (aList)
+  "Return average of a list"
   (cond (aList = NIL))
                        
 )
 
+; 5
 (defun isType (dataType) 
- 
+  "Return anonymous function checking if a value is the dataType"
 )
 
+; 6
 (defun taxCalculator (limit rate values)
+  "Find taxes"
  
 )
 
+; 7
 (defun clean (aFunc aList)
- 
+  "Clean list using function"
 )
 
-(defun threeWayBranch (x y toExecute)
+; 8
+(defmacro threeWayBranch (x y toExecute)
+ "Branching macro" 
  
 )
