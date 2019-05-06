92db9f262dfb57c0a4514a64170c95a1e3a932c6
John Naylor <jonaylor89@gmail.com>
2019-05-06 13:47:33 -0400

    Threading

--- Assignment7/RaceTrack.java
+++ Assignment7/RaceTrack.java
@@ -19,53 +19,73 @@ public class RaceTrack extends JPanel {
   private static Racer[] racers;
 
   public static void main(String argv[]) {
+
+    /**
+     * Initalize window settings
+     */
     frame = new JFrame("Race Track");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setResizable(false);
 
-    racers[0] = new Racer();
-    racers[1] = new Racer();
-    racers[2] = new Racer();
-    runRacers();
 
+    /**
+     * Create the track to place objects
+     */
     RaceTrack track = new RaceTrack();
-    frame.add(track);
+    frame.setContentPane(track);
+
+    /**
+     * Add buttons to the screen for starting and stopping the race
+     */
+    JPanel panel = new JPanel();
+    panel.setLayout(new FlowLayout());
 
     startButton = new JButton("Start");
     startButton.addActionListener(e -> {
-      System.out.println("Start");
       startRacers();
     });
-    frame.getContentPane().add(startButton);
+    panel.add(startButton);
 
     pauseButton = new JButton("Pause");
     pauseButton.addActionListener(e -> {
-      System.out.println("Pause");
       pauseRacers();
     });
-    frame.getContentPane().add(pauseButton);
+    panel.add(pauseButton);
 
     resetButton = new JButton("Reset");
     resetButton.addActionListener(e -> {
-      System.out.println("Reset");
       pauseRacers();
       resetDistances();
     });
 
-    frame.getContentPane().add(resetButton);
-
-    frame.setLayout(new FlowLayout());
+    panel.add(resetButton);
 
     try {
       car = ImageIO.read(new File("sportive-car.png"));
-      JLabel label = new JLabel(new ImageIcon(car));
-      frame.add(label);
     } catch (IOException ex) {
       System.out.println("IO error");
       System.exit(1);
     }
 
+    frame.add(panel);
 
+    /**
+     * Create racers for the track
+     */
+    racers = new Racer[3];
+
+    racers[0] = new Racer();
+    racers[1] = new Racer();
+    racers[2] = new Racer();
+
+    /**
+     * Start racer threads to wait for the start
+     */
+    runRacers();
+
+    /**
+     * Size and show the window
+     */
     frame.setSize(500, 200);
     frame.setVisible(true);
 
@@ -75,24 +95,24 @@ public class RaceTrack extends JPanel {
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
 
-    // Draw the images at the beginning of the track
-    g.drawImage(car, 0, 0, this);
-    g.drawImage(car, 0, 50, this);
-    g.drawImage(car, 0, 100, this);
-
     // Draw the 3 rectangle for the "tracks"
-    g.drawRect(230, 80, 10, 10);
-    g.drawRect(20, 20, 20, 20);
-    g.drawRect(30, 30, 30, 30);
+    g.setColor(Color.GRAY);
+    g.fillRect(25, 55, 400, 15);
+    g.fillRect(25, 95, 400, 15);
+    g.fillRect(25, 135, 400, 15);
 
-    g.setColor(Color.RED);
-    g.fillRect(10, 10, 100, 100);
+    // Draw the images at the beginning of the track
+    g.drawImage(car, racers[0].distance + 25, 50, this);
+    g.drawImage(car, racers[1].distance + 25, 90, this);
+    g.drawImage(car, racers[2].distance + 25, 130, this);
 
   }
 
   public static void resetDistances() {
     for (Racer r : racers) {
+      System.out.println("Reset " + r.go);
       r.distance = 0; 
+      r.go = false;
     }
   }
 
@@ -104,12 +124,14 @@ public class RaceTrack extends JPanel {
 
   public static void startRacers() {
     for (Racer r : racers) {
+      System.out.println("Start " + r.go);
       r.go = true; 
     }
   }
 
   public static void pauseRacers() {
     for (Racer r : racers) {
+      System.out.println("Pause " + r.go);
       r.go = false; 
     }
   }
@@ -125,9 +147,26 @@ class Racer extends Thread {
     go = false;
   }
 
+  @Override
   public void run() {
-    if (go) {
+    while (true) {
+      if (go) {
+
+        System.out.println("UMMMMMMM");
+
+        if (distance < 100) {
+          System.out.println("Updating distance " + distance);
+          distance += 5;
+        }
+        // Check to make sure I'm not at the max
+        // int deltaX = rand.nextInt(11);
     
+        try {
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+          return;
+        }
+      }
     }
   }
 }
